1f61cfe77fa300468b41dfa0609b5913f080b499
CameronStClair95 <cameronaitcheson@gmail.com>
2023-01-25 15:59:20 +0000

    Added text to Login and Sign Up. Make sure you download the dotenv & bcrypt package, I also tried to figure out the sign up route but i didn't work just yet

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,30 @@
 const User = require('../models/User.model');
+
 const Event = require('../models/Event.model');
+
 const router = require('express').Router();
+
 const bcrypt = require('bcryptjs');
+
+const saltRounds = 10 
+
 const mongoose = require('mongoose');
+
+// const { isLoggedIn, isLoggedOut} = require('../middeleware/route-guard.js');
+
+
+router.get('/signup', (req, res) => {
+    console.log(req.session)
+    // data = {userInsession:req.session.currentUser}
+    console.log(data)
+    res.render('auth/signup')
+})
 
 // Tomoz download bcryptjs (look into Hashedpassword)
 // Set up user pages & Routes
+
+// look into setting up the salt rounds
+// look into setting up the middleware - route-guard.js
 
 
 
