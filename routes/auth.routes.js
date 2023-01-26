5020b0e19e88c2b6e1198549b885e2ef7e931bbf
Nicole <92593875+ozzleme@users.noreply.github.com>
2023-01-26 10:47:46 +0530

    added route and hbs for event-details page

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -11,23 +11,25 @@ const mongoose = require('mongoose');
 // this route has the same action of the post form in create hbs page 
 
 router.post('/add-event', (req, res) => {
-const {eventName, description, category, price, image, city} = req.body
-Event.create({eventName, description, category, price, image, city})
-.then((result) => {
-    console.log("event has been created")
-    res.redirect('/events')
-    })
+    const { eventName, description, category, price, image, city } = req.body
+    Event.create({ eventName, description, category, price, image, city })
+        .then((result) => {
+            console.log("event has been created")
+            res.redirect('/events')
+        })
+        .catch((err) => console.log(err));
 })
 
 // read the document by finding all the events and render them
 
 router.get('/events', (req, res) => {
     Event.find()
-    .then((result)=> {
-        const eventsObject = {result}
-        console.log(eventsObject)
-        res.render('events/event-list', eventsObject)
-    })
+        .then((result) => {
+            const eventsObject = { result }
+            console.log(eventsObject)
+            res.render('events/event-list', eventsObject)
+        })
+        .catch((err) => console.log(err));
 })
 
 // create the document and make the form visible to used to add an event 
@@ -36,12 +38,22 @@ router.get('/add-event', (req, res) => {
     res.render('events/create')
 })
 
+// read one document using params 
+
+router.get('/events/:eventId', (req, res) => {
+    Event.findById(req.params.eventId)
+        .then((singleEvent) => {
+            res.render('events/event-details', singleEvent)
+        })
+        .catch((err) => console.log(err));
+})
+
 // create the currentUser
 
 router.get('/signup', (req, res) => {
     console.log(req.session)
     let data = {}
-    data = {userInsession:req.session.currentUser}
+    data = { userInsession: req.session.currentUser }
     console.log(data)
     res.render('auth/signup', data)
 })
