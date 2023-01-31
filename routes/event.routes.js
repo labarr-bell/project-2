7e0b632fe4e13135b6e8a34c7df0cec5fe40ad24
Nicole <92593875+ozzleme@users.noreply.github.com>
2023-01-31 23:21:29 +0530

    user profile page updated but still needs attention to work

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,14 +1,14 @@
 const User = require('../models/User.model');
 const Event = require('../models/Event.model');
 const router = require('express').Router();
-const {isAdmin} = require('../middleware/route-guard.js');
+// const {isAdmin} = require('../middleware/route-guard.js');
 const Trip = require('../models/Trip.model');
 
 
 // create the event routes 
 // this route has the same action of the post form in create hbs page 
 
-router.post('/add-event', isAdmin, (req, res) => {
+router.post('/add-event', (req, res) => {
     const { eventName, description, category, price, image, city } = req.body
     Event.create({ eventName, description, category, price, image, city })
         .then((result) => {
@@ -30,7 +30,7 @@ router.get('/events', (req, res) => {
 
 // create the document and make the form visible to used to add an event 
 
-router.get('/add-event', isAdmin, (req, res) => {
+router.get('/add-event', (req, res) => {
     res.render('events/create')
 })
 
@@ -51,16 +51,6 @@ router.get('/events/:eventId', (req, res) => {
         })
         .catch((err) => console.log(err));
 })
-
-// router.post('/events/:id/delete', async (req, res) => {
-//     try {
-//         const deletedEvent = await Event.findByIdAndDelete(req.params.id);
-//         res.redirect('/events');
-//     } catch (error) {
-//         console.log(error);
-//         res.redirect('/events');
-//     }
-// });
 
 router.post('/events/:eventId/delete', (req, res, next) => {
     Event.findByIdAndRemove(req.params.eventId)
