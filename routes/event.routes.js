907dd1493c1ddc5c0c96fa39509d77ebc001989b
CameronStClair95 <cameronaitcheson@gmail.com>
2023-02-04 16:03:30 +0000

    added some stylings/animations

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -6,6 +6,7 @@ const Trip = require('../models/Trip.model');
 const events = require("../config/events.json");
 const axios = require('axios').default;
 const fileUploader = require('../config/cloudinary.config');
+const _ = require('lodash.groupby');
 
 
 
