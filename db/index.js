26937aec8feac168d942242e910e87bb1fa5e968
CameronStClair95 <cameronaitcheson@gmail.com>
2023-02-07 17:51:36 +0000

    changed the mongodb url

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/pmongodb+srv://labarr-bell:1234@cluster0.jefqq6s.mongodb.net/travel-app?retryWrites=true&w=majorityroject-2";
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/project-2";
 
 mongoose
   .connect(MONGO_URI)
